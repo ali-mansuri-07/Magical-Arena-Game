71298b81345037dc11bbc2655745843fb843495d
ali-mansuri-07 <110375581+ali-mansuri-07@users.noreply.github.com>
2024-06-08 00:51:21 +0530

    Player class created

---
+++ Player.java
@@ -0,0 +1,27 @@
+public class Player {
+    int health;
+    int strength;
+    int attack;
+
+    public Player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+}
